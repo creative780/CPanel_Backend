28a660113a5faa437b76f9fe8019b493130d4b85
Abdullah <abdullah.work027@gmail.com>
2025-10-27 23:42:59 +0500

    settings 2

--- backend/settings.py
+++ backend/settings.py
@@ -156,7 +156,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # ---------------------------
 # CORS / CSRF
 # ---------------------------
-CORS_ALLOW_ALL_ORIGINS = env_bool("CORS_ALLOW_ALL_ORIGINS", False)
+CORS_ALLOW_ALL_ORIGINS = env_bool("CORS_ALLOW_ALL_ORIGINS", True)
 
 if not CORS_ALLOW_ALL_ORIGINS:
     CORS_ALLOWED_ORIGINS = [
